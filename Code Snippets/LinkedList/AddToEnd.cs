81a0e5a282bbd27c8a62fb839c669af107baa880
Velinhas <35391473+kipras21@users.noreply.github.com>
2020-06-14 00:02:31 +0300

    Update AddToEnd.cs

--- Code Snippets/LinkedList/AddToEnd.cs
+++ Code Snippets/LinkedList/AddToEnd.cs
@@ -2,17 +2,19 @@
 
 public void Add(T data)
 {
-	if (begin == null)
+	if (begin == null) //If list is empty
 	{
-		begin = new Node(data, null);
+		begin = new Node(data, null); //Set begin to data
 
 		end = begin;
+		//Count++
 	}
-	else
+	else //If list isn't empty
 	{
-		Node temp = new Node(data, null);
+		Node temp = new Node(data, null); //Create a temp node
 
-		end.Next = temp;
-		end = temp;
+		end.Next = temp; //Set it after the last current node
+		end = temp; //Set the last node as the added node
+		//Count++
 	}
 }
