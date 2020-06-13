5a51d296953aedb115edb855613b83fa0f07a053
Velinhas <35391473+kipras21@users.noreply.github.com>
2020-06-14 00:10:39 +0300

    Update IndexOf.cs

--- Code Snippets/LinkedList/IndexOf.cs
+++ Code Snippets/LinkedList/IndexOf.cs
@@ -1,16 +1,16 @@
-public int IndexOf(type obj)
+public int IndexOf(type obj) //Find the index of node with given data
 {
-    Node<type> current = this.HeadNode;
+    Node<type> current = this.HeadNode; //Set the list to start from beggining
 
     for (int i = 0; i < this.Count; i++)
     {
-        if (current.Data.Equals(obj))
+        if (current.Data.Equals(obj)) //If Node data is equal to given data
         {
-            return i;
+            return i; //Return the index
         }
 
-        current = current.Next;
+        current = current.Next; //Move to the next node
     }
 
-    return -1;
+    return -1; //Return -1 if index was not found
 }
